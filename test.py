93c9f16c2b68cc2574252d4a0b2354fadb67284f
Pratyay1010 <59337443+Pratyay1010@users.noreply.github.com>
2022-12-12 00:23:10 +0530

    Completed Project With Flask Implemented

---
+++ test.py
@@ -0,0 +1,15 @@
+from tensorflow.keras.models import load_model
+
+loaded_model = load_model('model.h5')
+
+z = loaded_model.predict([[0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+       0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+       1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+       0, 0, 0, 0, 0, 0, 1, 0]])
+
+z = z>=0.5
+if z[0][0]==True:
+       print("true")
+else:
+       print("false")
